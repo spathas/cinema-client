97c8e96dce0c30b7d3477f15bb6e8da7192991fd
spathas <spathasx@gmail.com>
2021-10-20 09:37:38 +0300

    Finish screen

--- src/components/booking/confirmation/FinishedScreen.js
+++ src/components/booking/confirmation/FinishedScreen.js
@@ -1,11 +1,31 @@
 import { useState, useEffect, useContext } from "react";
 
+//MUI
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
 //CONTEXTES
 import BookingContext from "../../../store/booking-context";
 import AuthContext from "../../../store/auth-context";
 
+//STYLES
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: "30vh",
+    flexGrow: 1,
+    background: theme.palette.dark,
+  },
+  divider: {
+    background: theme.palette.divider,
+  },
+}));
+
 function FinishedScreen() {
   const [bookingId, setBookingId] = useState();
+
+  const classes = useStyles();
 
   const authContext = useContext(AuthContext);
   const bookingContext = useContext(BookingContext);
@@ -37,7 +57,27 @@ function FinishedScreen() {
       .catch((error) => console.log(error));
   }, []);
 
-  return <div>test</div>;
+  return (
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="center"
+      className={classes.container}
+      spacing={3}
+    >
+      <Grid item md={6} align="center">
+        <Typography color="primary" variant="h6">
+          Booking id
+        </Typography>
+      </Grid>
+      <Grid item md={12}>
+        <Typography color="primary" variant="h5" align="center">
+          Booking id! I have to deploy the projects or fix the issue with cors
+          and cookies.
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default FinishedScreen;
